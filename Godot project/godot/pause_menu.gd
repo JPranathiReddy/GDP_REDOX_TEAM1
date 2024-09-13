c6c5b570cb5d36d606b978718f46b6bbf65b1fbf
sumanthyeruva <yeruva.sumanth07@gmail.com>
2024-09-13 12:33:33 -0500

    exit

--- Godot project/godot/pause_menu.gd
+++ Godot project/godot/pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 @onready var pause_menu = $"."
-
+@onready var exit_menu = "res://exit_conformation.tscn"
 # Called when the node enters the scene tree for the first time.
 
 
@@ -11,4 +11,5 @@ func _on_resumebtn_pressed():
 	
 
 func _on_quitbtn_pressed():
+	
 	get_tree().change_scene_to_file("res://menu.tscn")

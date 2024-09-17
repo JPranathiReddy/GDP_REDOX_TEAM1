6a73c52a53f41dab795333504e6c496b1eadea4a
Sai Krishna Reddy Seelam <143019716+Skr299@users.noreply.github.com>
2024-09-17 13:58:23 -0500

    Progress Bar Level - 1
    Committed Progress Bar Implementation changes for Level - 1

--- Godot project/godot/hud.gd
+++ Godot project/godot/hud.gd
@@ -1,11 +1,19 @@
 extends CanvasLayer
 
+##Onready Function for the implemetnion of ProgressBar
+@onready var progress_bar = $ProgressBar  # Reference to the ProgressBar node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
+# Initialized the progress bar value with the current progress from GlobalVars
+	progress_bar.value = GlobalVars.progress
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta): 
 	$Score.text = "Score: " + str(GlobalVars.score)
+	$ProgressBar.value = GlobalVars.progress  # Update the progress bar value
+
+
+	

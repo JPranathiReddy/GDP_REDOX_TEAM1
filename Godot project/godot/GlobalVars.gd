46b820982b57c37663ab8e4d61bd6464b7df3d5c
BandiRamNiteeshReddy <143049772+BandiRamNiteeshReddy@users.noreply.github.com>
2024-09-18 10:07:47 -0500

    added references lines

--- Godot project/godot/GlobalVars.gd
+++ Godot project/godot/GlobalVars.gd
@@ -13,7 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+#reset fuctuon ro reset game
 func _reset_game():
 	score = 0
 	levelSelected=0
